cde54eefc456438d9e5b4ec4a516d7177e43a6d6
Subhajit098 <subhajitdey2388@gmail.com>
2025-09-07 00:13:14 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+
+  // By default , vite outputs hashed filenames(bad for extensions). This ensures files are predictable (index.js, not index-xyz123.js)
+  build: {  
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
+      }
+    }
+  }
+})
