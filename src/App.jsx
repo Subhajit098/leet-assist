37d07b6bd5c05035a75510f61485dfa4ff7cf728
Subhajit098 <subhajitdey2388@gmail.com>
2025-09-14 02:08:18 +0530

    removed the URL showup in the pop up UIpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,40 @@
 import React, { useEffect, useState } from "react";
 
 function App() {
-  const [url, setUrl] = useState(null); // state to store the URL received 
 
 
-  useEffect(() => {
-    // 1. Read the latest URL when popup loads
-    chrome.storage.local.get("latestQuestionUrl", (result) => {
-      if (result.latestQuestionUrl) {
-        console.log("📂 Loaded from storage:", result.latestQuestionUrl);
-        setUrl(result.latestQuestionUrl);
-      }
-    });
+  // const [url, setUrl] = useState(null); 
 
-    // 2. Listen for storage changes (if new URL is stored)
-    function handleStorageChange(changes, areaName) {
-      if (areaName === "local" && changes.latestQuestionUrl) {
-        console.log("🔄 Storage updated:", changes.latestQuestionUrl.newValue);
-        setUrl(changes.latestQuestionUrl.newValue);
-      }
-    }
 
-    chrome.storage.onChanged.addListener(handleStorageChange);
-  }, [url]);
+  // useEffect(() => {
+  //   // 1. Read the latest URL when popup loads
+  //   chrome.storage.local.get("latestQuestionUrl", (result) => {
+  //     if (result.latestQuestionUrl) {
+  //       console.log("📂 Loaded from storage:", result.latestQuestionUrl);
+  //       setUrl(result.latestQuestionUrl);
+  //     }
+  //   });
 
-  console.log("URL : ",url)
+  //   // 2. Listen for storage changes (if new URL is stored)
+  //   function handleStorageChange(changes, areaName) {
+  //     if (areaName === "local" && changes.latestQuestionUrl) {
+  //       console.log("🔄 Storage updated:", changes.latestQuestionUrl.newValue);
+  //       setUrl(changes.latestQuestionUrl.newValue);
+  //     }
+  //   }
+
+  //   chrome.storage.onChanged.addListener(handleStorageChange);
+  // }, [url]);
+
+  // console.log("URL : ",url)
 
   return (
     <div style={{ padding: "1rem", width: "280px" }}>
       <h2>🚀 LeetCode Buddy</h2>
 
       {/* Button to trigger injection of content.js and fetch URL */}
-      <button >{url}</button>
+      <button >See hints !</button>
 
-      {/* Make an API call here once you have the URL (inside useEffect) */}
-      {url && (
-        <div style={{ marginTop: "1rem" }}>
-          <p>
-            🔗{" "}
-            {/* Always add rel="noopener noreferrer" when using target="_blank" for security */}
-            <a href={url} target="_blank" rel="noopener noreferrer">
-              {url}
-            </a>
-          </p>
-        </div>
-      )}
 
     </div>
   );
