da653d535c036d58da9a3a2a9a9f912fb0546bb5
Subhajit098 <subhajitdey2388@gmail.com>
2025-09-14 22:04:14 +0530

    API response is being successfully fetched

--- public/background.js
+++ public/background.js
@@ -1,10 +1,15 @@
+import OpenAI from "openai";
+const client = new OpenAI({
+    apiKey: import.meta.env.VITE_API_KEY
+});
+import {fetchData} from './utils/apiCall.js'
+
 console.log("Background service worker loaded");
 let url;
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === "QUESTION_URL") {
     url=message.url
-    console.log("📩 Received URL from content.js:", message.url);
-
+    console.log("📩 Received URL from content.js at :", message.url);
 
 
     // Respond back to the content script
@@ -15,6 +20,17 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     chrome.storage.local.set({ latestQuestionUrl: message.url }, () => {
       console.log("✅ URL saved in storage:", message.url);
     });
+
+
+    // make the API call here
+    fetchData(url)
+    .then((data) => {
+      console.log("Response from AI:", data);
+    })
+    .catch((err) => {
+      console.log("Some error occurred:", err);
+    });
+
 
 
     // Keep the message channel open (safe practice in MV3)
